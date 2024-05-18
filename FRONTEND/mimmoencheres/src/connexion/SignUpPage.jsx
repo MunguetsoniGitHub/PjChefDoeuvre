bb0ab65696b54069c855d8c6e895618148a8be49
MunguetsoniGitHub <emmanueletsoni6@gmail.com>
2024-05-18 18:28:34 +0200

    Déplacement du contenu du dossier BACKEND dans la branche backend

--- FRONTEND/mimmoencheres/src/connexion/SignUpPage.jsx
+++ FRONTEND/mimmoencheres/src/connexion/SignUpPage.jsx
@@ -27,14 +27,29 @@ const SignUpPage = () => {
         <h2 className="text-2xl mb-4">Créer un compte</h2>
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-4">
+            <input type="text" name="prenom" placeholder="Prénom" {...register('prenom', { required: true })} className="border border-gray-300 p-2 w-full rounded" />
+            {errors.prenom && <span className="text-red-500">Ce champ est requis</span>}
+          </div>
+          <div className="mb-4">
             <input type="text" name="nom" placeholder="Nom" {...register('nom', { required: true })} className="border border-gray-300 p-2 w-full rounded" />
             {errors.nom && <span className="text-red-500">Ce champ est requis</span>}
           </div>
           <div className="mb-4">
-            <input type="text" name="postenom" placeholder="Postnom" {...register('postenom', { required: true })} className="border border-gray-300 p-2 w-full rounded" />
-            {errors.postenom && <span className="text-red-500">Ce champ est requis</span>}
+            <input type="text" name="postnom" placeholder="Postnom" {...register('postnom', { required: true })} className="border border-gray-300 p-2 w-full rounded" />
+            {errors.postnom && <span className="text-red-500">Ce champ est requis</span>}
           </div>
-          {/* Ajouter les autres champs du modèle Register ici */}
+          <div className="mb-4">
+            <input type="text" name="adresse" placeholder="Adresse" {...register('adresse', { required: true })} className="border border-gray-300 p-2 w-full rounded" />
+            {errors.adresse && <span className="text-red-500">Ce champ est requis</span>}
+          </div>
+          <div className="mb-4">
+          <input type="text" name="numeroTel" placeholder="Numéro de téléphone" {...register('numeroTel', { required: true, pattern: /^\d{10}$/ })} className="border border-gray-300 p-2 w-full rounded" />
+            {errors.numeroTel && <span className="text-red-500">Ce champ est requis</span>}
+          </div>
+          <div className="mb-4">
+            <input type="date" name="dateNaissance" placeholder="Date de naissance" {...register('dateNaissance', { required: true })} className="border border-gray-300 p-2 w-full rounded" />
+            {errors.dateNaissance && <span className="text-red-500">Ce champ est requis</span>}
+          </div>
           <div className="mb-4">
             <input type="password" name="motDePasse" placeholder="Mot de passe" {...register('motDePasse', { required: true })} className="border border-gray-300 p-2 w-full rounded" />
             {errors.motDePasse && <span className="text-red-500">Ce champ est requis</span>}
@@ -43,7 +58,7 @@ const SignUpPage = () => {
             <input type="password" name="confirmationMotDePasse" placeholder="Confirmation mot de passe" {...register('confirmationMotDePasse', { required: true })} className="border border-gray-300 p-2 w-full rounded" />
             {errors.confirmationMotDePasse && <span className="text-red-500">Ce champ est requis</span>}
           </div>
-          {/* Autres champs du formulaire */}
+          
           <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Créer un compte</button>
         </form>
         <div className="mt-4">

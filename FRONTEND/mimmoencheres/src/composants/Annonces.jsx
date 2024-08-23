cebf13d900990c1803dbfdd69dfd3eb8a70190d9
MunguetsoniGitHub <emmanueletsoni6@gmail.com>
2024-08-23 12:05:22 +0100

    correction de l'erreur dans l'url de verification de participation

--- FRONTEND/mimmoencheres/src/composants/Annonces.jsx
+++ FRONTEND/mimmoencheres/src/composants/Annonces.jsx
@@ -45,7 +45,7 @@ const Annonce = ({ annonce, annonceId,
 
     const checkParticipation = async () => {
       try {
-        const response = await axios.get(`https://pjchefdoeuvre.onrender.com/${enchereId}/participation/${user.id}`);
+        const response = await axios.get(`https://pjchefdoeuvre.onrender.com/api/enchere/${enchereId}/participation/${user.id}`);
         setIsParticipant(response.data.isParticipant);
       } catch (error) {
         console.error(error);

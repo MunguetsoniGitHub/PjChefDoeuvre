5b50578ee62f1b6c6ae675c2afeb361bb315ed40
MunguetsoniGitHub <emmanueletsoni6@gmail.com>
2024-08-30 17:14:59 +0100

    Responsive  page d'accueil

--- FRONTEND/mimmoencheres/src/UIStandard/Accueil.jsx
+++ FRONTEND/mimmoencheres/src/UIStandard/Accueil.jsx
@@ -9,46 +9,69 @@ const Accueil = () => {
 
 
     return(
-    <div className=" flex flex-col h-max bg-gray-100">
-      <div className="bg-gradient-to-r from-blue-500 to-green-500 text-white h-96 px-40 flex flex-row gap-6"  style={{ backgroundImage: 'url("https://res.cloudinary.com/maecd11/image/upload/v1720194581/maedev/lvrhhn7czavwtitav6u2.png")', backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }} >
+      <div className="flex flex-col h-full bg-gray-100 overflow-x-hidden">
+      <div
+  className="bg-gradient-to-r from-blue-500 to-green-500 text-white h-96 px-4 py-20 sm:px-20 lg:px-40 flex flex-col lg:flex-row gap-6 justify-center items-center relative"
+  style={{
+          backgroundImage: 'url("https://res.cloudinary.com/maecd11/image/upload/v1720194581/maedev/lvrhhn7czavwtitav6u2.png")',
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          // height: '500px', // Fixer la hauteur
+        }}
+      >
       
-        <div className="border-solid border-2 shadow-md rounded-lg p-4 h-48 w-450px absolute bottom-1/4 left-72 bg-gray-mae flex items-center">
-            <h3 className="text-red-mae font-bold mb-2 font-kaushan text-center text-3xl">Profiter d'une meilleure expérience sur notre plateforme des ventes immobilières aux enchères. </h3>
-        </div>
-        
-        <div className="border-solid border-2 shadow-md rounded-lg p-4 h-48 w-450px absolute bottom-1/4 right-72 bg-white flex items-center">
-            <h3 className="text-black font-bold mb-2 font-kaushan  text-center text-3xl">Vous avez donc la possibilité de l'enchérir à une vente ou de mettre en vente votre propriété. </h3>
-        </div>
+      <div className="border-solid border-2 shadow-md rounded-lg p-4 w-96  bg-gray-mae flex items-center justify-center h-48">
+          <h3 className="text-red-mae font-bold text-2xl  font-kaushan text-center w-full">
+            Profitez d'une meilleure expérience sur notre plateforme des ventes immobilières aux enchères.
+          </h3>
+      </div>
+      {/* max-w-xs */}
+      <div className="border-solid border-2 shadow-md rounded-lg p-4 w-96  bg-white flex items-center justify-center h-48 hidden sm:flex">
+          <h3 className="text-black font-bold text-2xl font-kaushan text-center w-full">
+            Vous avez donc la possibilité de l'enchérir à une vente ou de mettre en vente votre propriété.
+          </h3>
+       </div>
       </div>
 
-      <div className="container mx-auto py-12 px-40 h-72 flex flex-row items-end">
-        <p>N'hésitez pas à dérouler les annonces afin d'apprécier les offres en lançant une recherche avec les options qui vous convient, et participer aux enchères d’une annonce en cliquant sur le bouton d'action "participer" et poursuivez cette annonce dans l'onglet/page "Compte" pour pouvoir enchérir avant la fin de cette dernière. </p>
+      <div className="container mx-auto py-12 px-4 sm:px-20 lg:px-40">
+        <p className="text-center">
+          N'hésitez pas à dérouler les annonces afin d'apprécier les offres en lançant une recherche avec les options qui vous conviennent, et participer aux enchères d’une annonce en cliquant sur le bouton d'action "participer" et poursuivez cette annonce dans l'onglet/page "Compte" pour pouvoir enchérir avant la fin de cette dernière.
+        </p>
       </div>
 
-      <div className="container mx-auto py-12 px-40 h-96 flex flex-col justify-around">
-        <h2 className="text-3xl font-bold mb-6 text-center">Pourquoi choisir MimmoEnchères</h2>
-        
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">       
+      <div className="container mx-auto py-12 px-4 sm:px-20 lg:px-40 flex flex-col gap-6">
+        <h2 className="text-xl font-bold mb-6 text-center">Pourquoi choisir MimmoEnchères</h2>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           <NousChoisir title="Annonces" lorem="Enchères transparentes et en temps réel pour une expérience d'achat immobilière immersive." />
           <NousChoisir title="Fiabilité" lorem="Accompagnement dans le processus d'achat, après paiement d'acompte." />
-          <NousChoisir title="Expérience utilisateur" lorem="Interface utilisateur intuitive et responsive pour une navigation fluide et agréable." />           
+          <NousChoisir title="Expérience utilisateur" lorem="Interface utilisateur intuitive et responsive pour une navigation fluide et agréable." />
         </div>
       </div>
 
-      <div className="container mx-auto gap-6 py-12 px-40 flex h-56 text-center items-center"  style={{ backgroundImage: 'url("https://res.cloudinary.com/maecd11/image/upload/v1720176204/maedev/zsrkqfagmxt5au6lfvc0.jpg")', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center'  }} >
-        
-        <div className="border-solid border-2 border-white shadow-md rounded-lg p-4">
-            <h3 className="text-white font-bold mb-2">Profiter d'une meilleure expérience sur notre plateforme des ventes immobilières aux enchères. </h3>
+      <div
+        className="container mx-auto gap-6 py-12 px-4 sm:px-20 lg:px-40 flex flex-col sm:flex-row justify-center items-center"
+        style={{
+          backgroundImage: 'url("https://res.cloudinary.com/maecd11/image/upload/v1720176204/maedev/zsrkqfagmxt5au6lfvc0.jpg")',
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <div className="border-solid border-2 border-white shadow-md rounded-lg p-4 max-w-xs w-full mb-4 sm:mb-0">
+          <h3 className="text-white font-bold text-center">
+            Profitez d'une meilleure expérience sur notre plateforme des ventes immobilières aux enchères.
+          </h3>
         </div>
-        
-        <div className="border-solid border-2 border-white shadow-md rounded-lg p-4">
-            <h3 className="text-white font-bold mb-2">Vous avez donc la possibilité de l'enchérir à une vente ou de mettre en vente votre propriété. </h3>
+
+        <div className="border-solid border-2 border-white shadow-md rounded-lg p-4 max-w-xs w-full">
+          <h3 className="text-white font-bold text-center">
+            Vous avez donc la possibilité de l'enchérir à une vente ou de mettre en vente votre propriété.
+          </h3>
         </div>
-        
       </div>
 
       <Footer />
-
     </div>
     )
 }
--- FRONTEND/mimmoencheres/src/composants/Accueil/NousChoisir.jsx
+++ FRONTEND/mimmoencheres/src/composants/Accueil/NousChoisir.jsx
@@ -5,7 +5,7 @@ function NousChoisir ({title, lorem})  {
 
     return(
         <div className="bg-white shadow-md rounded-lg p-4 flex flex-col gap-6">
-            <h3 className="text-xl font-bold mb-2 text-center">{title}</h3>
+            <h3 className="text-xl  mb-2 text-center">{title}</h3>
             <p className="text-gray-700">{lorem}</p>
         </div>
     );
